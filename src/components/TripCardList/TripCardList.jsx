c2870d4275af6ea6245044e0063d735afab5f797
victorcngo <hello@victorcango.fr>
2022-10-12 01:59:06 +0200

    refactor(TripCardList): remove useless styled component

--- src/components/TripCardList/TripCardList.jsx
+++ src/components/TripCardList/TripCardList.jsx
@@ -2,12 +2,6 @@ import React from 'react';
 import styled from 'styled-components';
 import { TripCard } from '../TripCard/TripCard';
 
-const TripCardListTitle = styled.h1`
-  font-size: 3.2rem;
-  margin-bottom: 3.2rem;
-  font-variation-settings: 'wght' 700;
-`;
-
 const TripCardWrapper = styled.div`
   display: grid;
   grid-template-columns: repeat(6, 1fr);
@@ -18,7 +12,6 @@ const TripCardWrapper = styled.div`
 export const TripCardList = ({ trips, addSelectTrip }) => {
   return (
     <>
-      <TripCardListTitle>All the Trips</TripCardListTitle>
       <TripCardWrapper>
         {trips.map((trip, i) => {
           return (
